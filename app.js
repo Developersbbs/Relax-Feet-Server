b004183c41f37d8a1754b0520980083e02621e9f
Sathish R <sathishravichandran9345@gmail.com>
2025-11-17 16:40:38 +0530

    after cors orgin changed

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ require('./config/firebaseAdmin');
 
 // CORS config
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin:  "http://app.relaxfeet.in/",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
